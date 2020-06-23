3b17544a3b88cf9008e4472d83c7a4235b4d7aa4
ninahorne <nina.horne1221@gmail.com>
2020-06-23 09:08:57 -0600

    crop="fill"

--- szn/src/app/app.component.ts
+++ szn/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'szn';
+  apiUrl = 'http://sznwithsalt.com/wp-json/wp/v2';
+  posts: any;
+
+  constructor(
+    private http: HttpClient
+  ) {}
+  ngOnInit() {
+    this.http.get(this.apiUrl + '/posts')
+    .subscribe(
+      posts => {
+        console.log(posts)
+        this.posts = posts;
+      }
+    )
+  }
+
 }
